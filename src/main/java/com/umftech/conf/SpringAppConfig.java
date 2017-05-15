cf034ecea56b693c0f777a58cd8728f8f9b42caf
Roger <zhuj02@gmail.com>
2017-05-15 14:16:30 -0700

    add no cache config

--- src/main/java/com/umftech/conf/SpringAppConfig.java
+++ src/main/java/com/umftech/conf/SpringAppConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -28,5 +30,15 @@ public class SpringAppConfig extends WebMvcConfigurerAdapter {
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
 	}
+	
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(0);
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(0);
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(0);
+        registry.addResourceHandler("/html/**").addResourceLocations("/html/").setCachePeriod(0);
+    }
+	
+
 
 }
